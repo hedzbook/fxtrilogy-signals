36801d9030a2ea17437dcf14d0586e02248ec5ea
hedzofficial <npkrdy@gmail.com>
2026-02-16 21:40:05 +0530

    Update 2026-02-16_21-40-04

--- app/page.tsx
+++ app/page.tsx
@@ -194,7 +194,7 @@ export default function Page() {
       {/* 🔥 BOTTOM CONTROL BAR — SAME HEIGHT */}
       <div className="fixed bottom-0 left-0 right-0 z-50 h-10">
 
-        <div className="bg-neutral-900 border-t border-neutral-800 h-full flex items-center justify-between px-[7px] shadow-[0_-8px_30px_rgba(0,0,0,0.6)]">
+        <div className="bg-neutral-900 border-t border-neutral-800 h-full flex items-center justify-between px-[15px] shadow-[0_-8px_30px_rgba(0,0,0,0.6)]">
 
           {/* ================= LEFT SIDE ================= */}
           <div className="flex items-center gap-2">
