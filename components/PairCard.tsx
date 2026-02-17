19821cae2d259f1c08bc8d0a458bf6fe0ac4e046
hedzofficial <npkrdy@gmail.com>
2026-02-18 02:24:43 +0530

    auto update 2026-02-18 02:24:42

--- components/PairCard.tsx
+++ components/PairCard.tsx
@@ -56,19 +56,21 @@ useEffect(() => {
   return (
 <div
   className={`
+    text-[clamp(10px,2.8vw,14px)]
     relative
     transition-all duration-300
     border border-neutral-800 rounded-xl
     overflow-hidden
     ${expanded 
-  ? "h-[calc(100vh-80px)]" 
-  : "h-[calc((100vh-80px-64px)/9)]"}
+      ? "h-[calc(100vh-80px)]" 
+      : "h-[calc((100vh-80px-64px)/9)]"}
     ${liveDir === "EXIT"
       ? "bg-gradient-to-b from-neutral-900 to-neutral-950 border-neutral-800/60"
       : "bg-[linear-gradient(180deg,rgba(20,20,20,0.9),rgba(0,0,0,0.95))]"
     }
   `}
 >
+
 
 {/* ================= HEADER ================== */}
 <div className="h-[72px] px-4 py-[6px] flex flex-col justify-between cursor-pointer"
